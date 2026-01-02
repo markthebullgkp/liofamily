21867f62d344f17856ee9cf092f250a2892d6db1
markthebullgkp <120301477+markthebullgkp@users.noreply.github.com>
2026-01-02 14:54:38 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -48,3 +48,7 @@ app.get("*", (req, res) => {
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get('/test', (req, res) => {
+  res.send('Backend OK');
+});
